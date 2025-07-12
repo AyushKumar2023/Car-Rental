ba2ef54a03db49bdb9a4746187b0f14b080608e0
AyushKumar2023 <ayushamber02@gmail.com>
2025-07-12 16:07:23 +0530

    first commit

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { assets, cityList } from '../assets/assets'
+
+function Hero() {
+
+    const [pickupLocation, setPicupLocation]=useState('')
+
+  return (
+    <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
+        
+        <h1 className='text-4xl md:text-5xl font-semibold'>Luxury cars on Rent</h1>
+
+        <form action="" className='flex flex-col md:flex-row items-start md:items-center
+        justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200
+        bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
+
+            <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
+                <div className='flex flex-col items-start gap-2'>
+                    <select required value={pickupLocation} onChange={(e)=>setPicupLocation(e.target.value)}>
+                        <option value="">Pickup Location</option>
+                        {cityList.map((city)=> <option key={city} value={city}>{city}</option>)}
+                    </select>
+                    <p className='px-1 text-sm text-gray-500'>{pickupLocation ? pickupLocation : 'Please select Location'}</p>
+                </div>
+
+                <div className='flex flex-col items-start gap-2'>
+                    <label htmlFor="pickup-date">Pick-up Date</label>
+                    <input type="date" name="" id="pickup-date" min={new Date().toISOString().split('T')[0]}
+                    className='text-sm text-gray-500' required/>
+                </div>
+
+                <div className='flex flex-col items-start gap-2'>
+                    <label htmlFor="return-date">Return Date</label>
+                    <input type="date" name="" id="return-date" className='text-sm text-gray-500' required/>
+                </div>
+
+            </div>
+
+            <button className='flex items-center justify-center gap-1 px-9 py-3
+            max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer'>
+                <img className='brightness-300' src={assets.search_icon} alt="search" />
+                 Search
+            </button>
+
+        </form>
+
+        <img className='max-h-74' src={assets.main_car} alt="car" />
+    </div>
+  )
+}
+
+export default Hero
