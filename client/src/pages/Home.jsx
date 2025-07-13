967b759f917161e3efec939fcac5545291674c46
AyushKumar2023 <ayushamber02@gmail.com>
2025-07-13 16:33:17 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
     <>
        <Hero/>
+       <FeaturedSection/>
+       <Banner/>
+       <Testimonial/>
+       <NewsLetter/>
     </>
   )
 }
